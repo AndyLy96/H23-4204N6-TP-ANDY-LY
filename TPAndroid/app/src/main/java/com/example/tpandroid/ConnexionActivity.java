a20953e450aef656f4309526b63c59868feb2111
Andy <2157338@cegepmontpetit.ca>
2023-02-03 10:28:23 -0500

    FCT : Activity Connexion
    Fonctionnement de la première activité

---
+++ TPAndroid/app/src/main/java/com/example/tpandroid/ConnexionActivity.java
@@ -0,0 +1,27 @@
+package com.example.tpandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.tpandroid.databinding.ConnexionMainBinding;
+
+public class ConnexionActivity extends AppCompatActivity {
+
+    ConnexionMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ConnexionMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.versInscription.setOnClickListener(view -> {
+            Intent i = new Intent(ConnexionActivity.this, InscriptionActivity.class);
+            startActivity(i);
+        });
+
+    }
+}
